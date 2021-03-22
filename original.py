a0472e93ccc260d6c502ea86c7a3dec1849ab2bc
Sergey Shevchenko <rootsergio@gmail.com>
2021-03-22 23:18:37 +0300

    sync socket server

---
+++ original.py
@@ -0,0 +1,27 @@
+"""
+Реализация простейшего серверного синхронного сокета
+"""
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 5000))
+server_socket.listen()
+
+while True:
+    print("Before .accept")
+    client_socket, addr = server_socket.accept()
+    print('Connection from', addr)
+
+    while True:
+        print("Before .recv()")
+        request = client_socket.recv(4096)
+
+        if not request:
+            break
+        else:
+            response = 'Hello world\n'.encode()
+            client_socket.send(response)
+
+    print("Outcide inner while loop")
+    client_socket.close()
